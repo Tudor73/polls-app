aa2053592ffe5e29a27ebea5db99a64770f479df
tminciunescu <tminciunescu@godaddy.com>
2022-08-10 22:02:03 +0300

    basic api routes and question input

--- src/pages/api/question.ts
+++ src/pages/api/question.ts
@@ -5,12 +5,19 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 const prisma = new PrismaClient()
 
 export default async function question(req: NextApiRequest, res: NextApiResponse<any>) {
-    await prisma.question.create({
-        data: {
-            description: "What is the meaning of life?",
-        }
-    })
-    res.status(200).json({ message: 'Question created!' })
+    if (req.method === 'POST') {
+        console.log(req.body)
+        await prisma.question.create({
+            data: {
+                description: "What is the meaning of life?",
+            }
+        })
+        res.status(200).json({ message: 'Question created!' })
+    }
+    else {
+        const questions = await prisma.question.findMany()
+        res.status(200).json(questions)
+    }
 }
 
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import { useRef, useState } from "react";
 
 const Home: NextPage = () => {
+  const question = useRef<HTMLInputElement>(null);
   return (
-    <div className="bg-slate-500">HEllo world</div>
-  )
-}
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center w-1/2 justify-around h-32">
+        <input
+          className="bg-white focus:outline-none border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal max-w-lg"
+          ref={question}
+        />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg w-32"
+          onClick={(e) => {
+            if (!question.current || question.current.value == "") return;
 
-export default Home
+            console.log(question.current.value);
+            question.current.value = "";
+          }}
+        >
+          Create
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
