036b0f2072cea5e697a9b30300b78a90ea9997d7
tminciunescu <tminciunescu@godaddy.com>
2022-08-11 20:58:41 +0300

    added options to db model

--- src/pages/api/question.ts
+++ src/pages/api/question.ts
@@ -1,28 +1,22 @@
-import { PrismaClient } from '@prisma/client'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../prisma/prisma";
 
-
-const prisma = new PrismaClient()
-
-export default async function question(req: NextApiRequest, res: NextApiResponse<any>) {
-    if (req.method === 'POST') {
-        await prisma.question.create({
-            data: {
-                description: req.body,
-            }
-        })
-        res.status(200).json({ message: 'Question created!' })
-    }
-    else {
-        const questions = await prisma.question.findMany()
-        res.status(200).json(questions)
-    }
+export default async function question(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  if (req.method === "POST") {
+    await prisma.question.create({
+      data: {
+        description: req.body.description,
+        options: req.body.options,
+        // votes: {},
+      },
+    });
+    res.status(200).json({ message: "Question created!" });
+  } else {
+    const questions = await prisma.question.findMany();
+    res.status(200).json(questions);
+  }
 }
-
-
-
-
-
-
-
-
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,71 @@
-import { Prisma } from "@prisma/client";
 import type { NextPage } from "next";
 import { useRef, useState } from "react";
+
+interface Question {
+  description: string;
+  options: string[];
+  votes?: number[];
+}
 
 const Home: NextPage = () => {
   const question = useRef<HTMLInputElement>(null);
 
+  const optionsRef = useRef<string[]>([]);
+
+  const [options, setOptions] = useState<string[]>([]);
+
   const handleQuestionCreate = async () => {
     if (!question.current || question.current.value == "") return;
-
+    const obj: Question = {
+      description: question.current.value,
+      options: optionsRef.current,
+    };
     const response = await fetch("/api/question", {
-      method: "GET",
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(question.current.value),
+      body: JSON.stringify(obj),
     });
     question.current.value = "";
+    setOptions([]);
   };
 
   return (
     <div className="flex flex-col items-center justify-around h-96">
       <h1 className="h-auto text-4xl">Polls app</h1>
-      <div className="flex flex-col items-center w-1/2 justify-around h-32">
+      <div className="flex flex-col items-center w-5/12 h-32">
         <input
-          className="bg-white focus:outline-none border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal max-w-lg"
+          placeholder="Question"
+          className="bg-white focus:outline-none border border-gray-300  rounded-lg py-4 px-4 block w-full appearance-none leading-normal text-lg"
           ref={question}
         />
+        {options.map((option, index) => {
+          return (
+            <input
+              key={index}
+              placeholder="Add option"
+              className="bg-white focus:outline-none border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+              ref={(element) => {
+                if (!element || element.value === "") return;
+                else {
+                  optionsRef.current[index] = element.value;
+                }
+              }}
+            />
+          );
+        })}
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg w-32"
+          className="bg-blue-500 hover:bg-blue-700 text-white  py-2 px-4 rounded-lg w-32 self-end"
+          onClick={(e) => {
+            console.log(optionsRef.current);
+            setOptions([...options, ""]);
+          }}
+        >
+          Add option
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg w-full"
           onClick={(e) => {
             e.preventDefault();
             handleQuestionCreate();
