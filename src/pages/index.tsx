dac9b06422713d01728af59f8fb6dfeacf129708
tudor73 <tudor@tudor.localdomain>
2022-09-10 15:21:29 +0300

    question form works

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,78 +1,84 @@
 import type { NextPage } from "next";
-import { useRef, useState } from "react";
+import React, { SyntheticEvent, useRef, useState } from "react";
+import { Field, FieldValues, useFieldArray, useForm } from "react-hook-form";
 
 interface Question {
   description: string;
   options: string[];
   votes?: number[];
 }
+type FormValues = {
+  question: string;
+  options: { optionId: string; value: string }[];
+};
 
 const Home: NextPage = () => {
-  const question = useRef<HTMLInputElement>(null);
+  const {
+    control,
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormValues>();
 
-  const optionsRef = useRef<string[]>([]);
+  const { fields, append, prepend, remove, swap, move, insert } = useFieldArray(
+    { control, name: "options" }
+  );
 
-  const [options, setOptions] = useState<string[]>([]);
-
-  const handleQuestionCreate = async () => {
-    if (!question.current || question.current.value == "") return;
+  const handleQuestionCreate = (data: FormValues) => {
     const obj: Question = {
-      description: question.current.value,
-      options: optionsRef.current,
+      description: data.question,
+      options: data.options
+        .map((option) => option.value)
+        .filter((value: string) => value !== ""),
     };
-    const response = await fetch("/api/question", {
+    const response = fetch("/api/question", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(obj),
     });
-    question.current.value = "";
-    setOptions([]);
+    reset(); //resets form
   };
 
   return (
-    <div className="flex flex-col items-center justify-around h-96">
-      <h1 className="h-auto text-4xl">Polls app</h1>
-      <div className="flex flex-col items-center w-5/12 h-32">
-        <input
-          placeholder="Question"
-          className="bg-white focus:outline-none border border-gray-300  rounded-lg py-4 px-4 block w-full appearance-none leading-normal text-lg"
-          ref={question}
-        />
-        {options.map((option, index) => {
-          return (
-            <input
-              key={index}
-              placeholder="Add option"
-              className="bg-white focus:outline-none border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
-              ref={(element) => {
-                if (!element || element.value === "") return;
-                else {
-                  optionsRef.current[index] = element.value;
-                }
-              }}
-            />
-          );
-        })}
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white  py-2 px-4 rounded-lg w-32 self-end"
-          onClick={(e) => {
-            console.log(optionsRef.current);
-            setOptions([...options, ""]);
-          }}
+    <div className="flex flex-col items-center justify-around h-screen">
+      <h1 className="h-auto text-5xl ">Polls app</h1>
+      <div className="flex flex-col items-center w-10/12 h-64 text-black justify-around">
+        <form
+          onSubmit={handleSubmit(handleQuestionCreate)}
+          className="flex flex-col items-center h-64 text-black justify-around"
         >
-          Add option
-        </button>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg w-full"
-          onClick={(e) => {
-            e.preventDefault();
-            handleQuestionCreate();
-          }}
-        >
-          Create
-        </button>
+          <input
+            placeholder="Question"
+            {...register("question", { required: true })}
+            className="bg-white focus:outline-none border border-gray-300  rounded-lg py-4 px-4 block w-full appearance-none leading-normal text-lg"
+          />
+          {errors.question && <span>This field is required</span>}
+          {fields.map((field, index) => {
+            return (
+              <input
+                key={field.id}
+                placeholder="Add option"
+                className="bg-white focus:outline-none border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+                {...register(`options.${index}.value`)}
+              />
+            );
+          })}
+          <button
+            type="button"
+            className="bg-violet-600 hover:bg-violet-700 text-white  py-2 px-4 rounded-lg w-32 self-end"
+            onClick={(e) => {
+              append({ optionId: `${fields.length + 1}`, value: "" });
+            }}
+          >
+            Add option
+          </button>
+          <button className="bg-violet-600 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded-lg w-full">
+            Create
+          </button>
+        </form>
       </div>
     </div>
   );
